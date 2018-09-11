f79b972d09e45d8496b7b0a4988aa59ae5d9ea15
Pepper <jenearly@gmail.com>
2018-09-11 16:31:33 -0400

    update with manifestjson and some minor fixes

--- build/js/onerror.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-mapFallback = function mapFallback() {
-    var mapF = document.getElementById("map");
-    mapF.innerHTML = "Sorry, this map application could not be loaded";
-    mapF.className = 'mapFallback';
-};
-//# sourceMappingURL=onerror.js.map
